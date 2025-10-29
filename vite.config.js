9269e46d391d34ecfe218e3d485eb1cdf71fc14b
Romil Dubey <romildubey03@gmail.com>
2025-10-29 12:44:43 +0530

    deploy attempt

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://blog-nest-coral.vercel.app/',
         secure: false,
       },
     },
